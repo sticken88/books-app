08b703d4110f8624c4856c9c894c1cc264f1320d
Matteo Sticco <matteo.sticco@gmail.com>
2016-04-23 12:25:38 +0200

    Added classes Quote, Book and Review

--- books_project/books_app/models.py
+++ books_project/books_app/models.py
@@ -1,5 +1,30 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+# Maybe Quote and Review should extend the same base class
+
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=80, primary_key=True)
+    author = models.CharField(max_length=30)
+    genre = models.CharField(max_length=30)
+    year = models.PositiveIntegerField()
+
+class Quote(models.Model):
+    book = models.ForeignKey('Book')
+    text = models.TextField()
+    username = models.ForeignKey(AUTH_USER_MODEL)
+    datetime_added = models.DateTimeField(auto_now_add=True)
+    upvote = models.PositiveIntegerField()
+
+
+class Review(models.Model):
+    book = models.ForeignKey('Book')
+    text = models.TextField()
+    username = models.ForeignKey(AUTH_USER_MODEL)
+    datetime_added = models.DateTimeField(auto_now_add=True)
+    upvote = models.PositiveIntegerField()
+
