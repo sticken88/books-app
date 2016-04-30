5fa6c99662c27c6890ff7af553cf80e7c349a7de
Matteo Sticco <matteo.sticco@gmail.com>
2016-04-30 14:47:51 +0200

    Added method 'books' to get all the books inside the DB

--- books_project/books_app/views.py
+++ books_project/books_app/views.py
@@ -1,3 +1,24 @@
+from django.template import RequestContext, loader
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from models import *
 
 # Create your views here.
+def index(request):
+    #return HttpResponse("Prova pagina Django")
+    template = loader.get_template('books_app/index.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
+
+def books(request):
+
+    # get the list of books
+    books_list = Book.objects.order_by("title")
+
+    template = loader.get_template('books_app/books.html')
+    context = RequestContext(request, {
+                                         'books_list': books_list
+                                      })
+    return HttpResponse(template.render(context))
+
