c46edcc0ba36baf8afc598392cbf572a14fa24c7
Matteo Sticco <matteo.sticco@gmail.com>
2016-04-30 14:49:35 +0200

    Added STATICFILES_DIRS, not entirely sure that's needed though

--- books_project/books_project/settings.py
+++ books_project/books_project/settings.py
@@ -133,3 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    '/home/matteo/Documents/Development/django-projects/books-app/books_project/books_app',
+)
